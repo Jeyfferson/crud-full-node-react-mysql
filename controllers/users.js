9f626778a27879196f6d4228a714cd039153d4d5
Jeyfferson <jeyffesilv@gmail.com>
2023-07-13 20:38:56 -0300

    testando

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,40 @@ const express = require('express');
 const router = express.Router();
 const db = require('./../db/models');
 
+//Visualizar recebendo parametro pelo ID
+router.get('/consult/:id', async (req, res) => {
+
+   //Receber o parametro ID
+   const { id } = req.params;
+   //console.log(id);
+
+   //Recuperar o registro depois de receber o parametro
+   const user = await db.Users.findOne({
+      //Indicar as colunas que quero recuperar
+      attributes:['id','nome','telefone'],
+
+      //Acrescentar a condição para indicar qual registro deve ser retornado do banco de dados
+      where: {id},
+   });
+   //console.log(user);
+
+   //Acessa o IF se encontrar o registro pelo ID passado
+   if(user){
+      return res.json({
+         user
+      })
+   }else{
+      return res.status(400).json({
+         message: "Usuario não encontrado"
+      })
+   }
+
+   res.json({
+      message: "Usuario encontrado"
+   });
+})
+
+//Cadastrar exemplo
 router.post("/add", async (req, res) => {
 
    var data = req.body;
@@ -19,6 +53,83 @@ router.post("/add", async (req, res) => {
          message: "Error registering"
       })
    })
+
+})
+
+//Listar
+router.get('/consult', async (req, res) => {
+
+   //Receber o número da página, quando não enviado o número da página é atribuido página 1
+   const { page = 1 } = req.query
+   //console.log(page) //consult?page=3
+
+   //Limite de registro em cada página 10
+   const limit = 10;
+
+   //variável com número da útlima página
+   var lastPag = 1
+
+   //contar a quantidade de registro no banco de dados
+   const countUsers = await db.Users.count()
+   //console.log(countUsers)
+
+   //Acessar IF quando encontrar registro no banco de dados
+   if(countUsers !== 0) {
+
+      //Calcular última página
+      lastPag = Math.ceil((countUsers / limit)); //arredondar para cima "ceil"
+
+   }else {
+      res.status(400).json({
+         message: "ERROR: Nenhum registro encontrado!"
+      })
+   }
+
+
+
+   //Recuperar dados de todos os usuarios
+   const users = await db.Users.findAll({
+
+      //Indicar quais colunas acessar na consulta
+      attributes: ['id', 'nome', 'email'],
+
+      //Ordenar os registro de como aparecer
+      order: [['id', 'ASC']],
+
+      //Calcular APARTI de qual registro deve retornar e o limite de registros
+      offset: Number((page * limit) - limit), // ex:(3*10) - 10 = 20
+      limit
+   });
+
+   //Acessa o IF se encontrar nos registro no banco de dados
+   if(users) {
+
+      //Criar um objeto com as informações da paginação
+      var pagination = {
+         //caminho
+         path: '/consult',
+         //página atual
+         page,
+         //Calcular pagina anterior
+         prev_page_url: page - 1 >= 1 ? page - 1 : false,
+         //Calcular proxima pagina
+         next_page_url: Number(page) + Number(1) > lastPag ? false : Number(page) + Number(1),
+         //Última página
+         lastPag,
+         //QTD total de registro
+         totally_record: countUsers,
+      }
+
+      //retorno os dados em formado de objeto json
+      res.json({
+         users,
+         pagination
+      })
+   }else {
+      res.status(400).json({
+         message: "Erro ao listar registros"
+      })
+   }
 
 })
 
